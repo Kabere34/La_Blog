010fc3eb5462276f3fdd5f746f96aeb257fd8605
Ivy Kabere <kabereivy@gmail.com>
2022-05-17 14:50:52 +0300

    add save picture function

--- pitch/routes.py
+++ pitch/routes.py
@@ -68,12 +68,6 @@ def save_picture(form_picture):
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/images', picture_fn)
-
-    output_size= (125,125)
-    i = Image.open(form_picture)
-    i.thumbnail(output_size)
-    i.save(picture_path)
-
     form_picture.save(picture_path)
 
     return picture_fn
