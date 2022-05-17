44653af175f61bf20cc42d586a3d93b4bb4726a8
Ivy Kabere <kabereivy@gmail.com>
2022-05-17 13:57:20 +0300

    Initial Setup

---
+++ pitch/request.py
@@ -0,0 +1,22 @@
+# from pitch import app
+# import urllib.request
+# import json
+# from .quote import Quotes
+
+
+# def get_quotes():
+#     with urllib.request.urlopen('http://quotes.stormconsultancy.co.uk/random.json') as url:
+#         data = url.read()
+#         get_json = json.loads(data)
+
+#         if get_json['author'] and get_json['quote']:
+
+#             author = get_json['author']
+#             quote = get_json['quote']
+
+#             new_quote = Quotes(author, quote)
+
+#             return new_quote
+
+#         else:
+#             return None
